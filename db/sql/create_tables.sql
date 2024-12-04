69d3ce9064f8a6fef7ed3bc3c1284058caff03b1
danil <vinniopo@mail.ru>
2024-12-04 17:29:42 +0700

    added sql create statements for supermarkets and categories tables

---
+++ db/sql/create_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS supermarkets (
+    supermarket_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+	category_id INT AUTO_INCREMENT PRIMARY KEY,
+	supermarket_id INT UNSIGNED NOT NULL,
+    inner_code VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    FOREIGN KEY categories_supermarket (supermarket_id)
+				REFERENCES supermarkets (supermarket_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT supermarket_id_inner_code_unique UNIQUE (supermarket_id, inner_code)
+);

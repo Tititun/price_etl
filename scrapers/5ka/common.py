d3dc92e72541121a5b5fe04c0727eb3c7bea838a
danil <vinniopo@mail.ru>
2024-12-05 11:18:04 +0700

    added common module to the first scraper for storing constants

---
+++ scrapers/5ka/common.py
@@ -0,0 +1,7 @@
+"""
+this module contains common constants used across different scripts
+in 5ka module
+"""
+
+# this name is used to retrieve supermarket_id from the database
+SUPERMARKET = 'Пятёрочка'

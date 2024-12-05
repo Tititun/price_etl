42dfcecfd4a18e614b2c19874ef9e8e57a886279
danil <vinniopo@mail.ru>
2024-12-05 11:18:39 +0700

    added fetch_supermarket_categories function to db module

--- db/mysql_functions.py
+++ db/mysql_functions.py
@@ -8,6 +8,8 @@ from dotenv import load_dotenv
 import mysql.connector
 from mysql.connector.abstracts import MySQLConnectionAbstract
 
+from scrapers.common import Category
+
 load_dotenv()
 
 
@@ -27,7 +29,7 @@ def fetch_supermarket_id(connection: MySQLConnectionAbstract, name: str)\
         -> Optional[int]:
     """
     fetches a supermarket's id by its name from the database
-    :param conn: MySQL connection
+    :param connection: MySQL connection
     :param str name: name of the supermarket
     :returns: id of the supermarket or None if not found
     """
@@ -38,6 +40,24 @@ def fetch_supermarket_id(connection: MySQLConnectionAbstract, name: str)\
         )
         result = cur.fetchone()
     return result[0] if result else None
+
+
+def fetch_supermarket_categories(
+        connection: MySQLConnectionAbstract, name: str) -> list[Category]:
+    """
+    fetches supermarket's categories using its name from the database
+    :param connection: MySQL connection
+    :param str name: name of the supermarket
+    :returns: list of Category objects
+    """
+    with connection.cursor(dictionary=True) as cur:
+        cur.execute(
+            'SELECT supermarket_id, category_id, inner_code, c.name AS name'
+            ' FROM categories c JOIN supermarkets s USING (supermarket_id)'
+            ' WHERE s.name=%s',
+            (name,)
+        )
+        return [Category(**res) for res in cur.fetchall()]
 
 
 if __name__ == '__main__':

a9c34a93a41fb233e73054e610716207b11e1f8b
danil <vinniopo@mail.ru>
2024-12-06 15:20:23 +0700

    added products and product_info create table sql statements

--- db/sql/create_tables.sql
+++ db/sql/create_tables.sql
@@ -12,3 +12,26 @@ CREATE TABLE IF NOT EXISTS categories (
 				REFERENCES supermarkets (supermarket_id) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT supermarket_id_inner_code_unique UNIQUE (supermarket_id, inner_code)
 );
+
+CREATE TABLE IF NOT EXISTS products (
+	product_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    category_id INTEGER NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    created_on DATE DEFAULT (CURRENT_DATE),
+    FOREIGN KEY product_category (category_id) REFERENCES categories (category_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE product_info (
+    product_id INTEGER NOT NULL,
+    observed_on DATE DEFAULT (CURRENT_DATE),
+    price DECIMAL(8, 2),
+    discounted_price DECIMAL(8, 2),
+    rating DECIMAL (3, 2),
+    rates_count MEDIUMINT UNSIGNED,
+    unit VARCHAR(100),
+    FOREIGN KEY product_info_product (product_id) REFERENCES products (product_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT product_observed_date_unique UNIQUE (product_id, observed_on)
+);

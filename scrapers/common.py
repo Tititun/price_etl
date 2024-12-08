6618af1df2939398f3d9f71766feb041d49ba665
danil <vinniopo@mail.ru>
2024-12-08 10:55:12 +0700

    added ProductInfo and Product pydantic BaseModel classes

--- scrapers/common.py
+++ scrapers/common.py
@@ -2,7 +2,8 @@
 This file contains common objects which can be used by many scrapers
 """
 
-import logging
+import datetime
+from decimal import Decimal
 from typing import Optional
 
 from pydantic import BaseModel
@@ -25,9 +26,31 @@ class Category(BaseModel):
     """
     this class is used to validate category data which
     is collected by scrapers.
-    It is also used when fetching the data from the database.
     """
     supermarket_id: Optional[int]
     category_id: Optional[int]
     inner_code: str
     name: str
+
+
+class ProductInfo(BaseModel):
+    """
+    this class represents product info which is associated with a product
+    """
+    product_id: int
+    observed_on: datetime.date
+    price: Optional[Decimal]
+    discounted_price: Optional[Decimal]
+    rating: Optional[Decimal]
+    rates_count: int = 0
+    unit: Optional[str]
+
+
+class Product(BaseModel):
+    """
+    this class represents a product
+    """
+    product_id: int
+    category_id: int
+    name: str
+    created_on: datetime.date

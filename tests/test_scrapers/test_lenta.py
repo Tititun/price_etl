0b27c57b4a7283984f4d06c69d2f0234d428a730
danil <vinniopo@mail.ru>
2024-12-22 11:03:59 +0700

    added tests for lenta, moved test data to scrapers/data directory

--- tests/test_scrapers/test_lenta.py
+++ tests/test_scrapers/test_lenta.py
@@ -1,5 +1,5 @@
 """
-this module contains tests that cover functions from pyaterochka scraper
+this module contains tests that cover functions from lenta scraper
 """
 
 import json
@@ -11,16 +11,16 @@ import requests
 
 from scrapers.common import (Category, Product, ProductInfo, ProductList,
                              RequestData, Supermarket, get_today_date)
-from scrapers.pyaterochka.catalogue import parse_categories
-from scrapers.pyaterochka.scraper import parse_data, request_data
-from scrapers.pyaterochka.common import SUPERMARKET_NAME
+from scrapers.lenta.catalogue import parse_categories
+from scrapers.lenta.scraper import parse_data, request_data
+from scrapers.lenta.common import SUPERMARKET_NAME
 
 
 # path to example catalogue and product list data
 EXAMPLE_CATALOGUE = (
-    Path(__file__).parent / 'data' / 'example_catalogue_pyaterochka.json')
+    Path(__file__).parent / 'data' / 'example_catalogue_lenta.json')
 EXAMPLE_PRODUCT_LIST = (
-    Path(__file__).parent / 'data' / 'example_product_list_pyaterochka.json')
+    Path(__file__).parent / 'data' / 'example_product_list_lenta.json')
 
 
 @pytest.fixture
@@ -41,17 +41,11 @@ def test_parse_categories(example_catalogue):
     """
     expected_result = [
         Category(supermarket_id=1, category_id=None,
-                 category_code='73C20455',
-                 name='Новый Год / Готовим оливье'),
+                 category_code='763',
+                 name='Колбаса, сосиски / Ветчина'),
         Category(supermarket_id=1, category_id=None,
-                 category_code='73C20456',
-                 name='Новый Год / Запекаем в духовке'),
-        Category(supermarket_id=1, category_id=None,
-                 category_code='73C10301',
-                 name='Готовая еда / Горячие напитки'),
-        Category(supermarket_id=1, category_id=None,
-                 category_code='73C9714',
-                 name='Готовая еда / Блинчики, сырники и каши'),
+                 category_code='1791',
+                 name='Бытовая техника / Кухонные машины и мясорубки'),
     ]
     supermarket=Supermarket(supermarket_id=1, name=SUPERMARKET_NAME)
     parsed_categories = parse_categories(example_catalogue, supermarket)
@@ -79,7 +73,7 @@ def category():
     return Category(
         supermarket_id=1,
         category_id=1,
-        category_code="someID",
+        category_code="12345",
         name="Some name",
         last_scraped_on=None
     )
@@ -106,7 +100,7 @@ def test_request_data_fails(category, monkeypatch):
     def mock_response(*args, **kwargs):
         return MockResponse(403)
 
-    monkeypatch.setattr(requests, 'get', mock_response)
+    monkeypatch.setattr(requests, 'post', mock_response)
 
     assert request_data(category) is None
 
@@ -125,40 +119,40 @@ def test_parse_data(category, example_product_list):
         items=[
             Product(
                 product_id=None,
-                product_code='4133363',
+                product_code='54761',
                 category_id=1,
-                name='Шоколадный батончик Snickers Super 80г',
-                url='https://5ka.ru/product/4133363',
+                name='Мармелад жевательный БОН ПАРИ Забавный медвежонок, 75г',
+                url='https://lenta.com/product/marmelad-zhevatelnyjj-bon-pari-medvedi-rossiya-75g-54761',
                 created_on=today,
                 product_info=ProductInfo(
                     product_id=None,
                     observed_on=today,
-                    price=Decimal("69.99"),
-                    discounted_price=None,
-                    rating=Decimal('4.94'),
-                    rates_count=47688,
-                    unit='80 г'
+                    price=Decimal('73.69'),
+                    discounted_price=Decimal('54.99'),
+                    rating=Decimal('4.8'),
+                    rates_count=12,
+                    unit='75г'
                 )
             ),
             Product(
                 product_id=None,
-                product_code='2138420',
+                product_code='55437',
                 category_id=1,
-                name='Шоколадный батончик Twix Xtra с карамелью 82г',
-                url='https://5ka.ru/product/2138420',
+                name='Зефир ШАРМЭЛЬ с ароматом ванили, 255г',
+                url='https://lenta.com/product/zefir-sharmel-s-aromatom-vanili-rossiya-255g-55437',
                 created_on=today,
                 product_info=ProductInfo(
                     product_id=None,
                     observed_on=today,
-                    price=Decimal("67.99"),
-                    discounted_price=Decimal("59.99"),
-                    rating=Decimal('4.94'),
-                    rates_count=28393,
-                    unit='82 г'
+                    price=Decimal('168.49'),
+                    discounted_price=Decimal('149.99'),
+                    rating=Decimal('4.8'),
+                    rates_count=78,
+                    unit='255г'
                 )
             ),
         ]
     )
     category = Category(supermarket_id=1, category_id=1,
-                        category_code='test_code', name='test_name')
+                        category_code='12345', name='test_name')
     assert parse_data(data_from_request, category) == expected_result
